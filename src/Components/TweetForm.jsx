582c32f820a739750ac6830fdb199e4008a566ce
mdcohen1993 <mdcohen1993@users.noreply.github.com>
2020-12-03 01:27:40 +0200

    ms2-wip-server

--- src/Components/TweetForm.jsx
+++ src/Components/TweetForm.jsx
@@ -1,72 +1,60 @@
 import { React, useState, useEffect } from 'react';
-import { Form, Button, Alert, Row } from 'react-bootstrap';
+import { Form, Button, Alert, Row, Spinner } from 'react-bootstrap';
 import './TweetForm.css'
+import TweetItem from './TweetItem'
+
 
 
 export default function TweetForm(props) {
-  const [tweets, setTweets] = useState([]);
   const [currentTweet, setCurrentTweet] = useState("");
+  const [loadSpinner, setLoadSpinner] = useState(false)
   const timeStamp = new Date(Date.now())
+  const url = "https://micro-blogging-dot-full-stack-course-services.ew.r.appspot.com/tweet";
+
 
   const addTweet = e => {
     e.preventDefault();
-    setTweets([
-      ...tweets,
-      {
-        text: currentTweet,
-        key: timeStamp.toISOString()
-      }
-    ]);
-    setCurrentTweet('')
-    console.log(currentTweet)
-  };
+    setLoadSpinner(true);
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ content: currentTweet, userName: 'Max', date: timeStamp })
+    };
+    fetch(url, requestOptions)
+      .then(response => response.json())
+      .then(data => {
+        setCurrentTweet(data);
+        setLoadSpinner(false)
+      })
+      .catch(e=>console.log(e))
+    
+  }
 
-  useEffect(()=>{
-    const data = localStorage.getItem('tweetArray');
-    if(data){
-      setTweets(JSON.parse(data));
-    }
-  }, [])
-
-  useEffect(()=>{
-    localStorage.setItem('tweetArray', JSON.stringify([...tweets])
-  )});
-
-
-  const tweetDisplay = tweets.map(tweet => {
-    return (
-      <div className='tweetDisplay' key={tweet.key}>
-        <div className='userName'>
-          Max
-          </div>
-      <div className='tweetText'>
-          {tweet.text}
-        </div>
-        <div className ='timestamp'>{tweet.key}</div>
-      </div>
-    )
-  })
 
   return (
     <>
       <Row className='justify-content-center'>
         <Form className='tweetForm' onSubmit={addTweet}>
           <Form.Group>
-            <Form.Control required className='textarea' as='textarea' type='tweet' placeholder='What you have in mind...' onChange={e => setCurrentTweet(e.target.value)} value={currentTweet.text} />
+            <Form.Control required className='textarea' as='textarea' name="tweet[currentTweet]" type='text' placeholder='What you have in mind...' onChange={(e) => setCurrentTweet(e.target.value)} />
           </Form.Group>
           <Button variant="primary" className='addBtn' type='submit' disabled={currentTweet.length > 140 && 'disabled'}>Tweet</Button>{' '}
-
           {currentTweet.length > 140 && <Alert variant='danger' className='alert'>
             The tweet can't contain more then 140 chars.
           </Alert>}
+          <Spinner className='loadSpinner' animation="border" variant="light" style={{display: loadSpinner ? 'block' : 'none'}}/>
         </Form>
       </Row>
       <Row className='justify-content-center'>
-        <div>
-          {tweetDisplay}
-        </div>
+        <TweetItem key={timeStamp} currentTweet={currentTweet}></TweetItem>
       </Row>
     </>
   )
 }
+
+
+
+
+
+
 
